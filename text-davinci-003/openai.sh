c8b79d436b7816e45d0c8fae02fa93c268cd4c9e
Glaudiston Gomes da Silva <glaudistong@gmail.com>
2023-02-13 11:08:26 -0300

    add retry when server fails

--- text-davinci-003/openai.sh
+++ text-davinci-003/openai.sh
@@ -5,6 +5,26 @@
 # You can use this file to set it:
 source ${HOME}/.ssh/env;
 
+check_response() {
+	input="$(cat)";
+	cache_dir="$1"
+	question="$2";
+	if [ "${input}" == "" ]; then
+		echo "ERROR: Empty response. deleting cache dir, and retry" >&2;
+		rm -fr "${cache_dir}"
+		ask_openai "${question}";
+	fi;
+
+	err=$(echo "${input}" | jq .error)
+	if [ "${err}" != "null" ] ;then
+		err_type="$(echo "$err" | jq .type)";
+		err_message="$(echo "$err" | jq .message)"
+		echo "ERROR: ${err_type}: ${err_message}. Retrying..." >&2;
+		rm -fr "${cache_dir}"
+		ask_openai "${question}";
+	fi
+	echo "${input}";
+}
 
 function ask_openai() {
 	text="$@";
@@ -12,15 +32,15 @@ function ask_openai() {
 	cache_dir="${HOME}/.openai/cache/${text_hash}";
 
 	if [ -e "${cache_dir}/response" ]; then
-	       cat "${cache_dir}/response";
 	       echo "# INFO: Response cached at [${cache_dir}/response]" >&2;
+	       cat "${cache_dir}/response" | check_response "$cache_dir" "$text";
 	       exit 0;
 	fi;
 	mkdir -p "${cache_dir}";
 
 	json_text_field="$( echo -n "${text}" | jq --raw-input --slurp )";
 	wordcount=$(echo -n "${text}" | wc -w);
-	tokens=$(( 4096 - ( wordcount +20 ) )); # we don't have an effective way to know the request tokens, it is the count after parsing and interpreted by the openai... so let's just pretend it will add 20 the input for now... 
+	tokens=$(( 4096 - ( wordcount +100 ) )); # we don't have an effective way to know the request tokens, it is the count after parsing and interpreted by the openai... so let's just pretend it will add 20 the input for now... 
 	echo -n '{
 		"model": "text-davinci-003", 
 		"prompt": '"${json_text_field}"', 
@@ -32,8 +52,11 @@ function ask_openai() {
 		-H "Content-Type: application/json" \
 		-H "Authorization: Bearer ${OPENAI_API_KEY}" \
 		-d "@${cache_dir}/request" \
-		2> "${cache_dir}/curl-stderr" |
-		tee "${cache_dir}/response";
+		2> "${cache_dir}/curl-stderr" \
+		> "${cache_dir}/response";
+
+	cat "${cache_dir}/response" | check_response "$cache_dir" "$text";
+
 }
 
 ask_openai "$@" | jq -r ".choices[].text"
